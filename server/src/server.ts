47315123f55a3563bc44ebfe3453b96674cfb5e8
Stefan Ginev <choephix.dev@gmail.com>
2020-02-03 21:27:40 +0000

    refa

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,130 +1,23 @@
+import SERVER_CONFIG from "./settings-server.json"
 import express from "express"
-import SLOTS from "./settings-slots.json"
-import CONFIG from "./settings-server.json"
+import slots from "./slots"
 
 const app = express()
 
-app.get( "/spin", (req, res) => {
-  const result = makeSpinResult()
+app.get( "/spin", ( req, res ) => {
+  const result = slots.makeSpinResult()
   const json = JSON.stringify( result )
   res.send( json )
 } )
 
-app.get( "/debug", (req, res) => {
+app.get( "/debug", ( req, res ) => {
   let s = ''
   for ( let i = 0 ; i < 100 ; i++ ) {
-    const result = makeSpinResult()
+    const result = slots.makeSpinResult()
     s += `</br> <b>${ result.match }</b> - ${ result.symbols.map( o => ` - ${ o } - ` ) }`
   }
   res.send( s )
 } )
 
-const PORT = process.env.PORT || CONFIG.port
+const PORT = process.env.PORT || SERVER_CONFIG.port
 app.listen( PORT, () => console.log( `Slots API Server listening on port ${ PORT }` ) )
-
-
-
-//// -- //// -- ////
-
-
-
-type MatchType = 'trio' | 'pair' | 'none'
-
-interface SymbolData {
-  name : string
-  icon : string
-  weight : number
-  reward : SpinReward
-  index? : number
-}
-
-interface SpinReward {
-  coins : number
-}
-
-interface SpinResult {
-  match : MatchType
-  symbols : [number,number,number]
-  reward : SpinReward
-}
-
-
-const makeSpinResult = () : SpinResult => 
-{
-  const symbols = SLOTS.symbols.map( ( symbol, i ) => { return { index : i, ...symbol } } )
-
-  const getRandomSymbol = ( weighted:boolean ) => {
-    const i = ! weighted ? ~~( Math.random() * symbols.length ) :
-              getWeightedRandomItem( symbols.map( ( symbol, i:number ) => i ), 
-                                     symbols.map( symbol => symbol.weight ) )
-    const symbol = symbols[ i ]                       
-    symbols.splice( i, 1 )
-    return symbol
-  }
-
-  const symbolsToShuffledIndices = ( symbols:[SymbolData,SymbolData,SymbolData] ) =>
-    symbols
-      .sort( () => Math.random() - 0.5 )
-      .sort( () => Math.random() - 0.5 )
-      .sort( () => Math.random() - 0.5 )
-      .sort( () => Math.random() - 0.5 ) //// quick and dirty shuffling
-      .map( symbol => symbol.index ) as [ number, number, number ]
-
-  const match = getWeightedRandomItem( Object.keys( SLOTS.weights ), 
-                                       Object.values( SLOTS.weights ) ) as MatchType
-
-  const result = { match }
-
-  if ( match === 'none' )
-  {
-    let symbol_1 = getRandomSymbol( false )
-    let symbol_2 = getRandomSymbol( false )
-    let symbol_3 = getRandomSymbol( false )
-    return {
-      ...result, 
-      symbols : symbolsToShuffledIndices( [ symbol_1, symbol_2, symbol_3 ] ) ,
-      reward : null ,
-    }
-  }
-
-  if ( match === 'pair' )
-  {
-    let symbol_1 = getRandomSymbol( true )    
-    let symbol_2 = getRandomSymbol( false )    
-    return {
-      ...result, 
-      symbols : symbolsToShuffledIndices( [ symbol_1, symbol_1, symbol_2 ] ) ,
-      reward : null ,
-    }
-  }
-
-  if ( match === 'trio' )
-  {
-    let symbol = getRandomSymbol( true )    
-    return {
-      ...result,
-      symbols : symbolsToShuffledIndices( [ symbol, symbol, symbol ] ),
-      reward : symbol.reward ,
-    }
-  }
-
-  throw new Error( `Invalid configuration - unrecognized match type ${ match }` )
-}
-
-const getWeightedRandomItem = <T>( list:T[], weights:number[] ) : T => 
-{
-  let total_weight = weights.reduce( ( prev, cur, i, arr ) => prev + cur )
-
-  let random_num = Math.random() * total_weight
-  let weight_sum = 0
-
-  for ( let i = 0; i < list.length; i++ ) 
-  {
-    weight_sum += weights[i]
-    weight_sum = +weight_sum.toFixed(2)
-      
-    if ( random_num <= weight_sum ) {
-      return list[ i ]
-    }
-  }
-}
---
+++ server/src/slots.ts
@@ -0,0 +1,104 @@
+import SLOTS from "./settings-slots.json"
+
+type MatchType = 'trio' | 'pair' | 'none'
+
+interface SymbolData {
+  name : string
+  icon : string
+  weight : number
+  reward : SpinReward
+  index? : number
+}
+
+interface SpinReward {
+  coins : number
+}
+
+interface SpinResult {
+  match : MatchType
+  symbols : [number,number,number]
+  reward : SpinReward
+}
+
+const getWeightedRandomItem = <T>( list:T[], weights:number[] ) : T => 
+{
+  let total_weight = weights.reduce( ( prev, cur, i, arr ) => prev + cur )
+
+  let random_num = Math.random() * total_weight
+  let weight_sum = 0
+
+  for ( let i = 0; i < list.length; i++ ) 
+  {
+    weight_sum += weights[i]
+    weight_sum = +weight_sum.toFixed(2)
+      
+    if ( random_num <= weight_sum ) 
+      return list[ i ]
+  }
+}
+
+const slots = new class {
+    public makeSpinResult () : SpinResult
+    {
+      const symbols = SLOTS.symbols.map( ( symbol, i ) => { return { index : i, ...symbol } } )
+    
+      const getRandomSymbol = ( weighted:boolean ) => {
+        const i = ! weighted ? ~~( Math.random() * symbols.length ) :
+                  getWeightedRandomItem( symbols.map( ( symbol, i:number ) => i ), 
+                                         symbols.map( symbol => symbol.weight ) )
+        const symbol = symbols[ i ]                       
+        symbols.splice( i, 1 )
+        return symbol
+      }
+    
+      const symbolsToShuffledIndices = ( symbols:[SymbolData,SymbolData,SymbolData] ) =>
+        symbols
+          .sort( () => Math.random() - 0.5 )
+          .sort( () => Math.random() - 0.5 )
+          .sort( () => Math.random() - 0.5 )
+          .sort( () => Math.random() - 0.5 ) //// quick and dirty shuffling
+          .map( symbol => symbol.index ) as [ number, number, number ]
+    
+      const match = getWeightedRandomItem( Object.keys( SLOTS.weights ), 
+                                           Object.values( SLOTS.weights ) ) as MatchType
+    
+      const result = { match }
+    
+      if ( match === 'none' )
+      {
+        let symbol_1 = getRandomSymbol( false )
+        let symbol_2 = getRandomSymbol( false )
+        let symbol_3 = getRandomSymbol( false )
+        return {
+          ...result, 
+          symbols : symbolsToShuffledIndices( [ symbol_1, symbol_2, symbol_3 ] ) ,
+          reward : null ,
+        }
+      }
+    
+      if ( match === 'pair' )
+      {
+        let symbol_1 = getRandomSymbol( true )    
+        let symbol_2 = getRandomSymbol( false )    
+        return {
+          ...result, 
+          symbols : symbolsToShuffledIndices( [ symbol_1, symbol_1, symbol_2 ] ) ,
+          reward : null ,
+        }
+      }
+    
+      if ( match === 'trio' )
+      {
+        let symbol = getRandomSymbol( true )    
+        return {
+          ...result,
+          symbols : symbolsToShuffledIndices( [ symbol, symbol, symbol ] ),
+          reward : symbol.reward ,
+        }
+      }
+    
+      throw new Error( `Invalid configuration - unrecognized match type ${ match }` )
+    }
+}
+
+export default slots
